30bf14d309e8c87194cdacde6e1ea898c7b7c6e6
JDSeiler <jseiler@radford.edu>
2021-07-28 21:53:58 -0400

    Add an Option type for my sanity

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type Option<T> = T | null;
+// export type Result<T, E extends Error> = T | E;
+
+export function unwrap<T>(o: Option<T>): T {
+    if (o !== null) {
+        return o;
+    } else {
+        throw new Error('Unwrap on null value');
+    }
+}
+
+export function isSome<T>(o: Option<T>): boolean {
+    return o !== null;
+}
+
+export function isNone<T>(o: Option<T>): boolean {
+    return o === null;
+}
