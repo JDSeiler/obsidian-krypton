afc3723520647221f095c2e1b81a2e31fe32e53f
JDSeiler <jseiler@radford.edu>
2021-07-31 15:02:36 -0400

    Factor out shared code

---
+++ src/services/files.ts
@@ -0,0 +1,21 @@
+import { App, Editor, TFile } from 'obsidian';
+
+export function getReplacementRange(app: App, editor: Editor, file: TFile, includeFrontMatter: boolean = true) {
+    const meta = app.metadataCache.getFileCache(file);
+    let startLine;
+    if (includeFrontMatter) {
+        startLine = 0;
+    } else {
+        startLine = (meta?.frontmatter?.position?.end?.line || -1) + 1;
+    }
+    const startPosition = {
+        line: startLine,
+        ch: 0
+    }
+    const endPosition = {
+        line: editor.lineCount(),
+        ch: 0
+    }
+
+    return {start: startPosition, end: endPosition}
+}
