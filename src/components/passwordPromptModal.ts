0b5c1a914888168ebd7dc92f688fc14c6c259c1a
JDSeiler <jseiler@radford.edu>
2021-08-08 15:21:51 -0400

    Fix linter error

--- src/components/passwordPromptModal.ts
+++ src/components/passwordPromptModal.ts
@@ -11,7 +11,7 @@ export default class PasswordPromptModal extends Modal {
     this.titleEl.setText('Enter your password');
   }
   
-  onOpen() {
+  onOpen(): void {
     /*
     ===============
     Create Elements
